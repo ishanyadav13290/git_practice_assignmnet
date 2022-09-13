9d28da5f3c662c4f4c888b01a5d33ccf85e28554
ishanyadav13290 <ishanyadav13290@gmail.com>
2022-09-13 12:00:44 +0530

    palindrome and prime problem added

---
+++ palindrome-problem.js
@@ -0,0 +1,10 @@
+let word=naman;
+let rev="";
+
+for(let i=0;i<word.length;i++){
+    rev=rev+i;
+} if(rev==word){
+    console.log("palindrome");
+} else {
+    console.log("not a palindrome");
+}
---
+++ prime-problem.js
@@ -0,0 +1,9 @@
+// prime problem
+let X=15;
+for(let i=0;i<=15;i++){
+    if(i%X===0 && i%1===0){
+        true
+    }
+} if(true){
+    console.log("prime")
+}
